50f03adf21a8feaa947690ee667375d46fd9ad94
Acesso Livre <Acesso Livre@BC7N278832>
2026-06-10 19:06:20 -0300

    .proprierts

--- SPG-Financas/src/main/java/br/com/empresaSGP/SPG_Financas/entity/ContasPagarEntity.java
+++ SPG-Financas/src/main/java/br/com/empresaSGP/SPG_Financas/entity/ContasPagarEntity.java
@@ -1,5 +1,27 @@
 package br.com.empresaSGP.SPG_Financas.entity;
 
-public class ContasPagarEntity {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ContasPagar")
+public class ContasPagarEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+
+	
 
 }
