ad3c3d03d64f0789e92a0e7ec36c381e33ff0ceb
Acesso Livre <Acesso Livre@BC7N278831>
2026-06-22 19:13:41 -0300

    sffsefesfs

--- SPG-Financas/src/main/java/br/com/empresaSGP/SPG_Financas/entity/ContasPagarEntity.java
+++ SPG-Financas/src/main/java/br/com/empresaSGP/SPG_Financas/entity/ContasPagarEntity.java
@@ -22,7 +22,7 @@ import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
-@Table(name = "ContasPagar", catalog = "[SPG-Financas]", schema = "dbo")
+@Table(name = "ContasPagar")
 public class ContasPagarEntity implements Serializable {
 
 	/**
